5feda4fc42ddc4f60bc88d77a0324e2278cea2fb
Michael Becker <21beckem8@gmail.com>
2026-04-09 15:44:42 -0600

    Added pause to build script so I can use the virtualEnv without waiting for full build

--- build-macos-in-virtual-env.sh
+++ build-macos-in-virtual-env.sh
@@ -264,15 +264,18 @@ printf "     cmake -G Ninja -DCMAKE_C_COMPILER=\"\$CC\" -DCMAKE_CXX_COMPILER=\"\
 printf "     ninja\n\n"
 printf "  The finished app will be at: dolphin/build/Binaries/Dolphin.app\n\n"
 
+
+ok "   VirtualEnv Setup Complete!"
+echo "Press [Enter] key to continue to build dolphin, or [Ctrl+C] to stop here and use the VirtualEnv"
+echo ""
+read -p "Waiting for input..."
+
 banner "Step 5: Activating toolchain"
 source "${ENV_SCRIPT}"
 
 CONDA="$HOME/dolphin-toolchain/miniforge3/bin/conda"
 SOURCE_DIR="$HOME/becker-dolphin-source"
 BUILD_DIR="$SOURCE_DIR/build"
-
-
-
 
 
 banner "Step 6: Downloading source code"
